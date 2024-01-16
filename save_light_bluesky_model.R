fdeab0a5cf0be8c30bdf2be8f0fedbc4ac20db82
Salim Damerdji <31995532+sdamerdji@users.noreply.github.com>
2024-01-16 12:46:37 -0800

    rm lfs pointer

--- save_light_bluesky_model.R
+++ save_light_bluesky_model.R
@@ -1,3 +1,43 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:3eb7945b18bcc4022b55dbb10777901dc4099f93a46da98f3307d13c8c50f4f4
-size 1170
+library(readr)
+library(readxl)
+
+# This file fits the blue sky model and then saves a lightweight version of the model
+# that can only be used for predictions. This is necessary so the R Shiny App
+# doesn't take forever to load.
+
+sf_history <- read_csv("./Blue Sky Code and Inputs/SF_Logistic_Data.csv")
+
+model <- glm(Developed ~ Historic + Residential_Dummy + Zillow_Price_Real + 
+               Const_FedReserve_Real + Envelope_1000 + Upzone_Ratio + zp_OfficeComm +
+               zp_DensRestMulti + zp_FormBasedMulti + zp_PDRInd + zp_Public + 
+               zp_Redev + zp_RH2 + zp_RH3_RM1, 
+             data = sf_history, family = 'binomial')
+
+stripGlmLR = function(cm) {
+  cm$y = c()
+  cm$model = c()
+  
+  cm$residuals = c()
+  cm$fitted.values = c()
+  cm$effects = c()
+  cm$qr$qr = c()  
+  cm$linear.predictors = c()
+  cm$weights = c()
+  cm$prior.weights = c()
+  cm$data = c()
+  
+  
+  cm$family$variance = c()
+  cm$family$dev.resids = c()
+  cm$family$aic = c()
+  cm$family$validmu = c()
+  cm$family$simulate = c()
+  attr(cm$terms,".Environment") = c()
+  attr(cm$formula,".Environment") = c()
+  
+  cm
+}
+
+model <- stripGlmLR(model)
+
+saveRDS(model, "light_model.rds")
