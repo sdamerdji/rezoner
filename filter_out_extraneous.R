66d72e795e26417bc61fb65c6ff4800945630a3f
Salim Damerdji <31995532+sdamerdji@users.noreply.github.com>
2024-02-01 11:33:00 -0800

    rm unnecessary columns from df

--- filter_out_extraneous.R
+++ filter_out_extraneous.R
@@ -10,8 +10,8 @@ parcels_to_exclude <- c('State of California Property', 'Under Water Lot')
 bad_parcels <- tax %>%
   filter((property_class_code_definition %in% parcels_to_exclude) | 
            (exemption_code_definition == 'Cemetary') | (block == '9900') |
-           (block == '0006' & lot == '001') | (block == '1300' & lot == '001'))
-  # these are for usf campuses. check what sf does          | (block = '1145' & lot = '003') | (block == '1107' & lot=='008'))
+           (block == '0006' & lot == '001') | (block == '1300' & lot == '001'))#  | 
+           #(block == '1145' & lot == '003') | (block == '1107' & lot=='008'))
 
 # Take union of points of bad parcels
 to_exclude <- st_union(bad_parcels)
@@ -30,4 +30,11 @@ filtered <- filtered[filtered$ex_height2024 < 1111,]
 # sum(!is.na(removed$M4_ZONING)) # Ok so one parcel is wrongly removed. need to add back after the fact
 
 st_write_feather(filtered, './four_rezonings_v4.feather')
-
+df <- st_read_feather('./four_rezonings_v4.feather')
+colnames(df)
+df <- df %>% select(-MapBlkLot_Master, -Developed,
+              -M4_height, -M5_height, -EX_USE,
+              -M1_CAP, -M2_CAP, -M3_CAP,
+              -M1_GP_TYPE, -M2_GP_TYPE, -M3_GP_TYPE, -VACANT, -primary_key)
+df <- df %>% select(-sup_dist_name, -EX_GP_TYPE, -EX_ZONING)
+st_write_feather(filtered, './four_rezonings_v4.feather')
